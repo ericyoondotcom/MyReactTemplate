bbbb779d54ab68fa555016c5ee22212b3756e51f
Eric Yoon <emjyoon@gmail.com>
2024-08-30 15:34:56 -0400

    CSS

--- my-web-app/src/components/HomePage.js
+++ my-web-app/src/components/HomePage.js
@@ -2,7 +2,6 @@ import React from "react";
 import style from "./stylesheets/HomePage.module.scss";
 
 const HomePage = () => {
-    console.log("HI")
     return (
         <div id={style.home_page}>
             <h1>Welcome to the home page!</h1>
